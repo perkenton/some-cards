58b75b90f3316d0062384d5fde4677d16680f457
Alexander Vasiliev <selado@ya.ru>
2022-03-23 02:23:36 +0500

    Добавлен и стилизован блок Main с тестовыми данными

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import styles from './Main.module.scss';
+import Card from '../Card/Card';
+import CardsList from '../CardsList/CardsList';
+
+
+export default function Main() {
+  const initialCards = [
+    {
+      name: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, vel. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor, vel.',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    },
+    {
+      name: 'Нургуш',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/khrebet-nurgush.jpg'
+    },
+    {
+      name: 'Тулиновка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/tulinovka.jpg'
+    },
+    {
+      name: 'Остров Желтухина',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/zheltukhin-island.jpg'
+    },
+    {
+      name: 'Владивосток',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/vladivostok.jpg'
+    }
+  ];
+
+
+
+  return (
+    <div className={ styles.main }>
+      <CardsList>
+        { initialCards.map((card) => <Card image={ card.link } caption={ card.name } />) }
+      </CardsList>
+    </div>
+  )
+}
