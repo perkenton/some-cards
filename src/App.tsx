63d7a04c9e11cfc987ce2dfb3796410182a0f3f9
Alexander Vasiliev <selado@ya.ru>
2022-03-23 02:24:09 +0500

    Исправлен компонент App и его стили

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import './App.css';
+import styles from './App.module.scss';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={ styles.app }>
+      <Header />
+      <Main />
     </div>
   );
 }
-
-export default App;
