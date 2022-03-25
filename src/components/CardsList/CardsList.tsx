a2fd9fa9e4a3262be0d54c8642e6f05e9d6b490c
Alexander Vasiliev <selado@ya.ru>
2022-03-26 02:35:30 +0500

    Добавлен тип пропсов, исправлены стили

--- src/components/CardsList/CardsList.tsx
+++ src/components/CardsList/CardsList.tsx
@@ -2,7 +2,7 @@ import React, { ReactElement } from 'react';
 import styles from './CardsList.module.scss';
 
 
-export default function CardsList(props: { children: ReactElement[] }) {
+export default function CardsList(props: { children: ReactElement | ReactElement[] }) {
 
   return (
     <div className={ styles.cardsList }>
