c87d50dd641665e64d85053497d11b14b9b56960
Alexander Vasiliev <selado@ya.ru>
2022-03-26 02:33:15 +0500

    Добавлены переменные для отправки запросов к API, неразрывный пробел и типы данных

---
+++ src/common/constants.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export const apiUrl = process.env.REACT_APP_API_URL;
+export const apiKey = process.env.REACT_APP_API_KEY;
+export const instance = axios.create({
+  baseURL: apiUrl,
+  headers: { 'Authorization': apiKey! }
+});
+
+export const NBS = '\xA0'; // Non-Breaking Space (неразрывный пробел)
