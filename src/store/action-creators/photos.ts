82ba1f09793f7f1a5f6671a569ec83644ad2f553
Alexander Vasiliev <selado@ya.ru>
2022-06-16 23:01:51 +0500

    Исправлен параметр url в запросе

--- src/store/action-creators/photos.ts
+++ src/store/action-creators/photos.ts
@@ -7,8 +7,8 @@ export const fetchPhotosAC = (page: number) => {
   return async (dispatch: Dispatch<PhotosActions>) => {
     try {
       dispatch({ type: PhotosActionTypes.FETCH_PHOTOS });
-      const response = await instance.get('curated', {params: { per_page: 10, page: page }});
-      dispatch({ type: PhotosActionTypes.FETCH_PHOTOS_SUCCESS, photos: response.data.photos })
+      const response = await instance.get('/photos/', {params: { per_page: 10, page: page }});
+      dispatch({ type: PhotosActionTypes.FETCH_PHOTOS_SUCCESS, photos: response.data })
     } catch (error: any) {
       dispatch({
         type: PhotosActionTypes.FETCH_PHOTOS_ERROR,
