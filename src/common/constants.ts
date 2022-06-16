627ed22c5d7448227c622a9406e80b901746b425
Alexander Vasiliev <selado@ya.ru>
2022-06-16 22:59:08 +0500

    Исправлен headers для запросов

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -4,7 +4,7 @@ export const apiUrl = process.env.REACT_APP_API_URL;
 export const apiKey = process.env.REACT_APP_API_KEY;
 export const instance = axios.create({
   baseURL: apiUrl,
-  headers: { 'Authorization': apiKey! }
+  headers: { 'Authorization': `Client-ID ${apiKey}` }
 });
 
 export const NBS = '\xA0'; // Non-Breaking Space (неразрывный пробел)
